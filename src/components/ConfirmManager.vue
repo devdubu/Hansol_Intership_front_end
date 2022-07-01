0c29531f66369bd5f7688b91c89d6fe24f8f08ff
devdubu <insomnis2020@naver.com>
2022-07-01 10:37:18 +0900

    Complete ConfirmManager Page

--- src/components/ConfirmManager.vue
+++ src/components/ConfirmManager.vue
@@ -1,3 +1,4 @@
+<!-- 완성 -->
 <template>
       <!--검색 부분 -->
       <div class="grow bg-slate-700	rounded-lg ml-2 mt-5">
@@ -65,7 +66,7 @@
                 <button class="mr-4 bg-orange-500 w-40 rounded-lg hover:bg-orange-600 active:bg-orange-700 focus:outline-none"><p class="mt-0.5 ml-0.5">미승인PM SMS전송</p></button>
                 <button class="mr-4 bg-red-500	w-16 rounded-lg	hover:bg-red-600 active:bg-red-700 focus:outline-none"><p class="mt-0.5 ml-0.5">승인취소</p></button>
                 <button class="mr-4 bg-yellow-500 w-10 rounded-lg	hover:bg-yellow-600 active:bg-yellow-700 focus:outline-none"><p class="mt-0.5 ml-0.5">반려</p></button>
-                <button class="mr-4 bg-emerald-500	w-10 rounded-lg	hover:bg-emerald-600 active:bg-emerald-700 focus:outline-none"><p class="mt-0.5 ml-0.5">승인</p></button>
+                <button @click="SendApprovalData" class="mr-4 bg-emerald-500	w-10 rounded-lg	hover:bg-emerald-600 active:bg-emerald-700 focus:outline-none"><p class="mt-0.5 ml-0.5">승인</p></button>
             </div>
           </div>
           <!-- 상단 검색 부분 끝 -->
@@ -77,14 +78,14 @@
             <div class="bg-slate-600 h-16 rounded-tl-lg rounded-tr-lg" style="max-width:1230px">
               <div class="flex">
                 <div class="pt-2 border-r" style="width:180px;"><p class="mt-3">구분</p></div>
-                <div class="pt-2 main-interval border-r" ><p>Mon <span><input type="checkbox"></span></p><p>{{viewDate[0]}}</p></div><!--0~6번 째로 해당 주차의 status를 구분한다.-->
-                <div class="pt-2 main-interval border-r" ><p>Tue <span><input type="checkbox"></span></p><p>{{ viewDate[1] }}</p></div>
-                <div class="pt-2 main-interval border-r" ><p>Web <span><input type="checkbox"></span></p><p>{{ viewDate[2] }}</p></div>
-                <div class="pt-2 main-interval border-r"><p>Thu <span><input type="checkbox"></span></p><p>{{ viewDate[3] }}</p></div>
-                <div class="pt-2 main-interval border-r" ><p>Fri <span><input type="checkbox"></span></p><p>{{ viewDate[4] }}</p></div>
-                <div class="pt-2 main-interval border-r"><p>Sat <span><input type="checkbox"></span></p><p>{{ viewDate[5] }}</p></div>
-                <div class="pt-2 main-interval border-r"><p>Sun <span><input type="checkbox"></span></p><p>{{ viewDate[6] }}</p></div>
-                <div class="pt-2 all" style="width: 70px;"><p class="mt-3">All <span><input type="checkbox"></span></p></div>
+                <div class="pt-2 main-interval border-r" ><p>Mon <span><input v-model="checkDate" :value="SendDate[0]" type="checkbox"></span></p><p>{{viewDate[0]}}</p></div><!--0~6번 째로 해당 주차의 status를 구분한다.-->
+                <div class="pt-2 main-interval border-r" ><p>Tue <span><input v-model="checkDate" :value="SendDate[1]" type="checkbox"></span></p><p>{{ viewDate[1] }}</p></div>
+                <div class="pt-2 main-interval border-r" ><p>Web <span><input v-model="checkDate" :value="SendDate[2]" type="checkbox"></span></p><p>{{ viewDate[2] }}</p></div>
+                <div class="pt-2 main-interval border-r"><p>Thu <span><input v-model="checkDate" :value="SendDate[3]" type="checkbox"></span></p><p>{{ viewDate[3] }}</p></div>
+                <div class="pt-2 main-interval border-r" ><p>Fri <span><input v-model="checkDate" :value="SendDate[4]" type="checkbox"></span></p><p>{{ viewDate[4] }}</p></div>
+                <div class="pt-2 main-interval border-r"><p>Sat <span><input v-model="checkDate" :value="SendDate[5]" type="checkbox"></span></p><p>{{ viewDate[5] }}</p></div>
+                <div class="pt-2 main-interval border-r"><p>Sun <span><input v-model="checkDate" :value="SendDate[6]" type="checkbox"></span></p><p>{{ viewDate[6] }}</p></div>
+                <div class="pt-2 all" style="width: 70px;"><p class="mt-3">All <span><input type="checkbox" v-model="checkDate" :value="SendDate" ></span></p></div>
               </div>
             </div>
 
@@ -134,10 +135,10 @@
                 <div class="flex">
                     <div class="border-r flex" style="width:180px;">
                         <div class=" pt-2" style="width:50%"><p>{{ member[0].memberName }}</p></div>
-                        <div class=" pt-2 border-l" style="width:50%"><input type="checkbox"> </div><!--멤버별 승인 제외 index로 구분한다.-->
+                        <div class=" pt-2 border-l" style="width:50%"><input v-model="checkOutMember" :value="teamMember[index]" type="checkbox"> </div><!--멤버별 승인 제외 index로 구분한다.-->
                     </div>
                     <div class="flex sub-interval border-r" >
-                        <div class=" hour-interval"><p class="pt-1">{{ member[0].totalTime }}</p></div>
+                        <div class=" hour-interval"><p class="pt-1">{{ member[0].dayHour }}</p></div>
                         <div class=" hour-interval border-l">
                             <div :class="{blind:statusFunc(member[0].signStatus)}, confirmCheck(member[0].signStatus)" class="w-10 rounded-xl state-interval">
                               <p class="mt-1">{{ confirmText[member[0].signStatus] }}</p>
@@ -153,7 +154,7 @@
                         </div>
                     </div>
                      <div class="flex sub-interval border-r" >
-                        <div class=" hour-interval"><p class="pt-1">{{ member[2].totalTime }}</p></div>
+                        <div class=" hour-interval"><p class="pt-1">{{ member[2].dayHour }}</p></div>
                         <div class=" hour-interval border-l">
                             <div :class="{blind:statusFunc(member[2].signStatus)}, confirmCheck(member[2].signStatus)" class="w-10 rounded-xl state-interval">
                               <p class="mt-1">{{ confirmText[member[2].signStatus] }}</p>
@@ -161,7 +162,7 @@
                         </div>
                     </div>
                      <div class="flex sub-interval border-r" >
-                        <div class=" hour-interval "><p class="pt-1">{{ member[3].totalTime }}</p></div>
+                        <div class=" hour-interval "><p class="pt-1">{{ member[3].dayHour }}</p></div>
                         <div class=" hour-interval border-l">
                             <div :class="{blind:statusFunc(member[3].signStatus)}, confirmCheck(member[3].signStatus)" class="w-10  rounded-xl state-interval">
                               <p class="mt-1">{{ confirmText[member[3].signStatus] }}</p>
@@ -169,7 +170,7 @@
                         </div>
                     </div>
                      <div class="flex sub-interval border-r" >
-                        <div class=" hour-interval"><p class="pt-1">{{ member[4].totalTime }}</p></div>
+                        <div class=" hour-interval"><p class="pt-1">{{ member[4].dayHour }}</p></div>
                         <div class=" hour-interval border-l">
                             <div :class="{blind:statusFunc(member[4].signStatus)}, confirmCheck(member[4].signStatus)" class="w-10  rounded-xl state-interval">
                               <p class="mt-1">{{ confirmText[member[4].signStatus] }}</p>
@@ -177,7 +178,7 @@
                         </div>
                     </div>
                      <div class="flex sub-interval border-r" >
-                        <div class=" hour-interval"><p class="pt-1">{{ member[5].totalTime }}</p></div>
+                        <div class=" hour-interval"><p class="pt-1">{{ member[5].dayHour }}</p></div>
                         <div class=" hour-interval border-l">
                             <div :class="{blind:statusFunc(member[5].signStatus)}, confirmCheck(member[5].signStatus)" class="w-10  rounded-xl state-interval">
                               <p class="mt-1">{{ confirmText[member[5].signStatus] }}</p>
@@ -185,7 +186,7 @@
                         </div>
                     </div>
                      <div class="flex sub-interval" >
-                        <div class=" hour-interval"><p class="pt-1">{{ member[6].totalTime }}</p></div>
+                        <div class=" hour-interval"><p class="pt-1">{{ member[6].dayHour }}</p></div>
                         <div class=" hour-interval border-l">
                             <div :class="{blind:statusFunc(member[6].signStatus)}, confirmCheck(member[6].signStatus)" class="w-10  rounded-xl state-interval">
                               <p class="mt-1">{{ confirmText[member[6].signStatus] }}</p>
@@ -193,7 +194,7 @@
                         </div>
                     </div>
                     <div class="flex" >
-                      <div class="border-l" style="width: 71px;"><p class="pt-2 pl-1">{{ member[0].dayHour }}</p></div>
+                      <div class="border-l" style="width: 71px;"><p class="pt-2 pl-1">{{ member[0].totalTime }}</p></div>
                     </div>
                 </div>
               </div>
@@ -202,7 +203,6 @@
             </div>
           </div>
 
-          
       </div>
 
 </template>
@@ -212,52 +212,85 @@ export default {
   name: 'CompanyProject',
   data(){
     return{
+      //원본 데이터 파일
       approval: approval,
+      //화면이 보여주는 영역
+
       Date: [],
       viewDate: [],
       teamMember: [],
       viewApprovalTable: [],
-      conform: "green",
-      confirmText: ['','','확정','팀장','마감']
+      confirmText: ['','','확정','팀장','마감'],
+
+      //checking 데이터
+      SendDate:[],
+      checkOutMember:[],
+      checkDate: [],
+
+
+      //보내주는 데이터 셋
+      CopyApprovalDate: [],
+      sendData:
+        {
+          "memberids": [],
+          "days": []
+        },
+
+
+
+
     }
   },
   mounted() {
-    this.calDate(this.approval[0].perfDay);
-    this.printDate();
-    this.calTeamMember(this.approval);
-    this.viewApprovalTableFunc(this.approval);
+    this.SetCopyData()
+    this.calDate(this.CopyApprovalDate[0].perfDay);
+    this.calTeamMember(this.CopyApprovalDate);
+    this.viewApprovalTableFunc(this.CopyApprovalDate);
   },
   methods:{
-   calDate(date){
+    //------------------------------------------- 마운트 되기 전에 실행되야하는 함수 --------------------------------
+
+    SetCopyData(){
+      for(var i = 0;i<this.approval.length;i++){
+        this.CopyApprovalDate.push({
+          "perfDay" : this.approval[i].perfDay,
+          "memberId": this.approval[i].memberId,
+          "memberName" : this.approval[i].memberName,
+          "signStatus": this.approval[i].signStatus,
+          "totalTime" : this.approval[i].totalTime,
+          "dayHour": this.approval[i].dayHour,
+          "is_Holiday": this.approval[i].is_Holiday
+        })
+      }
+    },
+    //------------------------------------------- 데이터를 화면에 출력해주는 함수들 --------------------------------
+
+    calDate(startdate){
      for(var i = 0;i<7;i++){
-       this.Date[i] = date+i;
+       this.Date[i] = startdate+i;
+       this.SendDate[i] = String(this.Date[i])
      }
-     console.log(this.Date);
-   },
-    printDate(){
-     var date = [...this.Date];
-     var year, month, day;
+
+
+      var date = [...this.Date];
 
       for(var i = 0;i<7;i++){
         date[i] = String(date[i]);
-        year = date[i].substr(0,4);
-        month = date[i].substr(4,2);
-        day = date[i].substr(6,2);
 
-        this.viewDate[i] = month+'.'+day;
+        this.viewDate[i] = [date[i].slice(4,6),'.',date[i].slice(6,8)].join('');
       }
-      console.log(this.viewDate)
-    },
+
+   },//멤버를 멤버 리스트에 넣는 작업
     calTeamMember(approval){
      for(var i = 0;i<approval.length;i++){
       if(this.Date[0] === approval[i].perfDay){
         this.teamMember.push(approval[i].memberId)
       }
      }
-     console.log(this.teamMember);
-    },
+     this.sendData.memberids = this.teamMember
+      console.log(this.teamMember)
+    },//화면에 멤버 리스트가 뽑히는 부분
     viewApprovalTableFunc(approval){
-
       for(var i = 0; i<this.teamMember.length;i++){
         var arr = []
         for(var j = 0; j< approval.length;j++){
@@ -268,7 +301,6 @@ export default {
         this.viewApprovalTable[i] = arr
       }
 
-      console.log(this.viewApprovalTable)
     },
     statusFunc(status){
       //색상이랑, p태그만 교체하면됨
@@ -284,7 +316,20 @@ export default {
         'bg-fuchsia-500':status === 3,
         'bg-rose-500': status === 4
       }
-    }
+    },
+    //--------------------------------------------------- 데이터를 수집하는 함수 --------------------------------
+    SendApprovalData(){
+      this.sendData.days = this.checkDate;
+      const member = this.sendData.memberids;
+      const checkoutMember = this.checkOutMember;
+
+      this.sendData.memberids = member.filter(x=>!checkoutMember.includes(x))
+      console.log(this.sendData)
+    },
+    CancelApprovalFunc(){
+
+    },
+
   },
   components: {
   }
